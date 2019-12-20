35f3ebc4495860fd779dceb5cae3bf7dc92e71c1
wy3 <wuuyi123@gmail.com>
2019-12-20 20:13:36 +0700

    update

--- src/parser.c
+++ src/parser.c
@@ -78,9 +78,31 @@ static void emit(uint32_t instruction)
 		parser.previous.line, parser.previous.column);
 }
 
+#define _OPCODE(x)					AUP_OP_##x
+
+#define EMIT_Op(op)					emit(_OPCODE(op))
+#define EMIT_OpA(op, A)				emit(AUP_SET_OpA(_OPCODE(op), A))
+#define EMIT_OpAx(op, Ax)			emit(AUP_SET_OpAx(_OPCODE(op), Ax))
+#define EMIT_OpAxCx(op, Ax, Cx)		emit(AUP_SET_OpAxCx(_OPCODE(op), Ax, Cx))
+
+#define EMIT_OpAB(op, A, B)			emit(AUP_SET_OpAB(_OPCODE(op), A, B))
+#define EMIT_OpABC(op, A, B, C)		emit(AUP_SET_OpABC(_OPCODE(op), A, B, C))
+
+#define EMIT_OpABx(op, A, Bx)		emit(AUP_SET_OpABx(_OPCODE(op), A, Bx))
+#define EMIT_OpABxCx(op, A, Bx, Cx)	emit(AUP_SET_OpABxCx(_OPCODE(op), A, Bx, Cx))
+
+#define EMIT_OpAsB(op, A, sB)		emit(AUP_SET_OpAsB(_OPCODE(op), A, sB))
+#define EMIT_OpAsC(op, A, sC)		emit(AUP_SET_OpAsC(_OPCODE(op), A, sC))
+#define EMIT_OpAsBsC(op, A, sB, sC)	emit(AUP_SET_OpAsBsC(_OPCODE(op), A, sB, sC))
+
 static void endCompiler()
 {
 	//emitReturn();
+}
+
+static void expression()
+{
+	// What goes here?      
 }
 
 bool aup_compile(const char *source, aupCh *chunk)
