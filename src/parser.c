6c84ecd9c081eb4d8b02c821a3c62c42bd48d009
wy3 <wuuyi123@gmail.com>
2019-12-21 10:37:17 +0700

    parsing '\!' unary

--- src/parser.c
+++ src/parser.c
@@ -194,6 +194,7 @@ static void unary()
 
 	// Emit the operator instruction.              
 	switch (operatorType) {
+		case TOKEN_BANG: //emitByte(OP_NOT); break;
 		case TOKEN_MINUS: //emitByte(OP_NEGATE); break;
 		default:
 			return; // Unreachable.                    
@@ -214,7 +215,7 @@ static ParseRule rules[] = {
     [TOKEN_SLASH]           = { NULL,     binary,  PREC_FACTOR },
     [TOKEN_STAR]            = { NULL,     binary,  PREC_FACTOR },
 
-    [TOKEN_BANG]            = { NULL,     NULL,    PREC_NONE },
+    [TOKEN_BANG]            = { unary,    NULL,    PREC_NONE },
     [TOKEN_BANG_EQUAL]      = { NULL,     NULL,    PREC_NONE },
     [TOKEN_EQUAL]           = { NULL,     NULL,    PREC_NONE },
     [TOKEN_EQUAL_EQUAL]     = { NULL,     NULL,    PREC_NONE },
