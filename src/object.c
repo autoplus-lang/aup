7c9f2c073da7c5f5999be9ea177bffa83a46d3c8
wy3 <wuuyi123@gmail.com>
2019-12-21 21:24:53 +0700

    add string hashing

--- src/object.c
+++ src/object.c
@@ -4,6 +4,18 @@
 #include "object.h"
 #include "memory.h"
 
+static uint32_t hashString(const char *key, int length)
+{
+	uint32_t hash = 2166136261u;
+
+	for (int i = 0; i < length; i++) {
+		hash ^= key[i];
+		hash *= 16777619;
+	}
+
+	return hash;
+}
+
 #define ALLOC_OBJ(type, objectType) \
 	(type *)allocObject(sizeof(type), objectType)
 
@@ -14,25 +26,29 @@ static aupO *allocObject(size_t size, aupVt type)
 	return object;
 }
 
-static aupOs *allocString(char *chars, int length)
+static aupOs *allocString(char *chars, int length, uint32_t hash)
 {
 	aupOs *string = ALLOC_OBJ(aupOs, AUP_TSTR);
 	string->length = length;
 	string->chars = chars;
+	string->hash = hash;
 
 	return string;
 }
 
 aupOs *aupOs_take(char *chars, int length)
 {
-	return allocString(chars, length);
+	uint32_t hash = hashString(chars, length);
+	return allocString(chars, length, hash);
 }
 
 aupOs *aupOs_copy(const char *chars, int length)
 {
+	uint32_t hash = hashString(chars, length);
+
 	char *heapChars = AUP_ALLOC(char, length + 1);
 	memcpy(heapChars, chars, length);
 	heapChars[length] = '\0';
 
-	return allocString(heapChars, length);
+	return allocString(heapChars, length, hash);
 }
