26be867f57eecb5bdf0385d06ca6ead3cf33fc46
wy3 <wuuyi123@gmail.com>
2019-12-21 21:12:44 +0700

    add string object

---
+++ src/object.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include <string.h>
+
+#include "object.h"
+#include "memory.h"
+
+#define ALLOC_OBJ(type, objectType) \
+	(type *)allocObject(sizeof(type), objectType)
+
+static aupO *allocObject(size_t size, aupVt type)
+{
+	aupO *object = (aupO *)aup_realloc(NULL, 0, size);
+	object->type = type;
+	return object;
+}
+
+static aupOs *allocString(char *chars, int length)
+{
+	aupOs *string = ALLOC_OBJ(aupOs, AUP_TSTR);
+	string->length = length;
+	string->chars = chars;
+
+	return string;
+}
+
+aupOs *aupOs_copy(const char *chars, int length)
+{
+	char *heapChars = AUP_ALLOC(char, length + 1);
+	memcpy(heapChars, chars, length);
+	heapChars[length] = '\0';
+
+	return allocString(heapChars, length);
+}
