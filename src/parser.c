95d98fbe3838d3876dd2c4058db195e5a7c9ab6c
wy3 <wuuyi123@gmail.com>
2019-12-21 12:57:44 +0700

    add error synchronization

--- src/parser.c
+++ src/parser.c
@@ -323,9 +323,38 @@ static void putsStatement()
 	//emitByte(OP_PRINT);
 }
 
+static void synchronize()
+{
+	parser.panicMode = false;
+
+	while (parser.current.type != TOKEN_EOF) {
+		if (parser.previous.type == TOKEN_SEMICOLON) return;
+
+		switch (parser.current.type) {
+			case TOKEN_CLASS:
+			case TOKEN_FUN:
+			case TOKEN_VAR:
+			case TOKEN_FOR:
+			case TOKEN_IF:
+			case TOKEN_WHILE:
+			case TOKEN_PUTS:
+			case TOKEN_RETURN:
+				return;
+
+			default:
+				// Do nothing.                                  
+				;
+		}
+
+		advance();
+	}
+}
+
 static void declaration()
 {
 	statement();
+
+	if (parser.panicMode) synchronize();
 }
 
 static void statement()
