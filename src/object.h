a1cb696e962bfd40f16bae8d9a07c138f99befdd
wy3 <wuuyi123@gmail.com>
2019-12-22 08:17:12 +0700

    add object, string macros

--- src/object.h
+++ src/object.h
@@ -16,6 +16,16 @@ struct _aupOs {
 	uint32_t hash;
 };
 
+static inline bool aupO_isType(aupV value, aupVt type) {
+	return AUP_IS_OBJ(value) && AUP_AS_OBJ(value)->type == type;
+}
+
+#define AUP_OBJ_TYPE(v)	(AUP_AS_OBJ(v)->type)
+#define AUP_IS_STR(v)	aupO_isType(v, AUP_TSTR)	
+
+#define AUP_AS_STR(v)	((aupOs *)AUP_AS_OBJ(v))         
+#define AUP_AS_CSTR(v)	(((aupOs *)AUP_AS_OBJ(v))->chars)
+
 aupOs *aupOs_take(AUP_VM, char *chars, int length);
 aupOs *aupOs_copy(AUP_VM, const char *chars, int length);
 
