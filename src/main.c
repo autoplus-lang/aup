d6fb99ce37a48f4da4ff881e9e0f97fb864cd5f2
wy3 <wuuyi123@gmail.com>
2019-12-29 15:05:20 +0700

    update main

--- src/main.c
+++ src/main.c
@@ -1,38 +1,6 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
 
-#include "aup.h"
-#include "vm.h"
-
-static char *readFile(const char* path)
-{
-	FILE *file = fopen(path, "rb");
-	if (file == NULL) {
-		fprintf(stderr, "Could not open file \"%s\".\n", path);
-		exit(74);
-	}
-
-	fseek(file, 0L, SEEK_END);
-	size_t fileSize = ftell(file);
-	rewind(file);
-
-	char *buffer = (char*)malloc(fileSize + 1);
-	if (buffer == NULL) {
-		fprintf(stderr, "Not enough memory to read \"%s\".\n", path);
-		exit(74);
-	}
-
-	size_t bytesRead = fread(buffer, sizeof(char), fileSize, file);
-	if (bytesRead < fileSize) {
-		fprintf(stderr, "Could not read file \"%s\".\n", path);
-		exit(74);
-	}
-
-	buffer[bytesRead] = '\0';
-	fclose(file);
-	return buffer;
-}
+#include "context.h"
 
 int main(int argc, char *argv[])
 {
@@ -41,19 +9,19 @@ int main(int argc, char *argv[])
 		return 0;
 	}
 	
-	char *source = readFile(argv[argc - 1]);
-	aupVM *vm = aupVM_new();
+    int ret = 0;
+    const char *file = argv[argc - 1];
 
-	clock_t clk = clock();
+    aupCtx *ctx = aup_create();
 
-	int ret = aup_interpret(vm, source);
-
-	if (ret != AUP_OK)
-		printf("\n%s error!\n", ret == AUP_COMPILE_ERR ? "Compile" : "Runtime");
-
-	printf("\ndone in %3gs!\n", (clock() - clk) / (double)CLOCKS_PER_SEC);
-
-	aupVM_free(vm);
-	free(source);
-	return 0;
+    if (!ctx == NULL) {
+        ret = aup_doFile(ctx, file);
+        aup_close(ctx);
+    }
+    else {
+        ret = 1;
+        fprintf(stderr, "Could not create new context!\n");
+    }
+	
+	return ret;
 }
