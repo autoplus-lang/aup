b43f63c003f8ef1337776313ee1105250c9bfd6d
wy3 <wuuyi123@gmail.com>
2020-01-10 10:17:17 +0700

    implement objects

---
+++ src/object.c
@@ -0,0 +1,145 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "object.h"
+#include "gc.h"
+#include "vm.h"
+
+void aup_printObject(aupObj *object)
+{
+    switch (object->type) {
+        case AUP_TSTR: {
+            aupStr *string = (aupStr *)object;
+            printf("%.*s", string->length, string->chars);
+            break;
+        }
+        case AUP_TFUN: {
+            aupFun *function = (aupFun *)object;
+            if (function->name == NULL)
+                printf("<script>");
+            else
+                printf("fn: %s", function->name->chars);
+            break;
+        }
+        case AUP_TMAP:
+            printf("map: %p", object);
+            break;
+        default:
+            printf("obj: %p", object);
+            break;
+    }
+}
+
+const char *aup_typeofObject(aupObj *object)
+{
+    switch (object->type) {
+        case AUP_TSTR:
+            return "str";
+        case AUP_TFUN:
+            return "fn";
+        default:
+            return "obj";
+    }
+}
+
+#define ALLOC(gc, size) \
+    aup_realloc(gc, NULL, 0, size)
+
+#define FREE(gc, type, pointer) \
+    aup_realloc(gc, pointer, sizeof(type), 0)
+
+#define ALLOC_OBJ(gc, type, objectType) \
+    (type *)allocObj(gc, sizeof(type), objectType)
+
+static aupObj *allocObj(aupGC *gc, size_t size, aupOType type)
+{
+    aupObj *object = ALLOC(gc, size);
+    object->type = type;
+
+    object->next = gc->objects;
+    gc->objects = object;
+    return object;
+}
+
+static aupStr *allocStr(aupGC *gc, aupTab *strings, char *chars, int length, uint32_t hash)
+{
+    aupStr *string = ALLOC_OBJ(gc, aupStr, AUP_TSTR);
+    string->chars = chars;
+    string->length = length;
+    string->hash = hash;
+
+    aup_setTable(strings, string, AUP_NIL);
+    return string;
+}
+
+aupStr *aup_takeString(aupVM *vm, char *chars, int length)
+{
+    uint32_t hash = aup_hashBytes(chars, length);
+    aupStr *interned = aup_findString(vm->strings, chars, length, hash);
+    if (interned != NULL) {
+        free(chars);
+        return interned;
+    }
+
+    return allocStr(vm->gc, vm->strings, chars, length, hash);
+}
+
+aupStr *aup_copyString(aupVM *vm, const char *chars, int length)
+{
+    uint32_t hash = aup_hashBytes(chars, length);
+    aupStr *interned = aup_findString(vm->strings, chars, length, hash);
+    if (interned != NULL) return interned;
+
+    char *heapChars = malloc((length + 1) * sizeof(char));
+    memcpy(heapChars, chars, length);
+    heapChars[length] = '\0';
+
+    return allocStr(vm->gc, vm->strings, heapChars, length, hash);
+}
+
+aupFun *aup_newFunction(aupVM *vm, aupSrc *source)
+{
+    aupFun *function = ALLOC_OBJ(vm->gc, aupFun, AUP_TFUN);
+
+    function->arity = 0;
+    function->name = NULL;
+    aup_initChunk(&function->chunk, source);
+
+    return function;
+}
+
+aupMap *aup_newMap(aupVM *vm)
+{
+    aupMap *map = ALLOC_OBJ(vm->gc, aupMap, AUP_TMAP);
+
+    aup_initHash(&map->hash);
+    aup_initTable(&map->table);
+
+    return map;
+}
+
+void aup_freeObject(aupGC *gc, aupObj *object)
+{
+    switch (object->type) {
+        case AUP_TSTR: {
+            aupStr *string = (aupStr *)object;
+            free(string->chars);
+            FREE(gc, aupStr, string);
+            break;
+        }
+        case AUP_TFUN: {
+            aupFun *function = (aupFun *)object;
+            aup_freeChunk(&function->chunk);
+            FREE(gc, aupFun, function);
+            break;
+        }
+        case AUP_TMAP: {
+            aupMap *map = (aupMap *)object;
+            aup_freeHash(&map->hash);
+            aup_freeTable(&map->table);
+            FREE(gc, aupMap, map);
+            break;
+        }
+    }
+}
---
+++ src/object.h
@@ -0,0 +1,57 @@
+#pragma once
+
+#include "value.h"
+#include "code.h"
+#include "table.h"
+
+#define AUP_OBJBASE aupObj obj
+
+struct _aupObj {
+    aupOType type;
+    aupObj *next;
+};
+
+struct _aupStr {
+    AUP_OBJBASE;
+    char *chars;
+    int length;
+    uint32_t hash;
+};
+
+struct _aupFun {
+    AUP_OBJBASE;
+    aupStr *name;
+    aupChunk chunk;   
+    int arity;
+};
+
+struct _aupMap {
+    AUP_OBJBASE;
+    aupTab table;
+    aupHash hash;
+};
+
+#define AUP_OBJTYPE(v)  (AUP_AS_OBJ(v)->type)
+#define AUP_IS_STR(v)   (aup_isObject(v, AUP_TSTR))
+#define AUP_IS_FUN(v)   (aup_isObject(v, AUP_TFUN))
+#define AUP_IS_MAP(v)   (aup_isObject(v, AUP_TMAP))
+
+#define AUP_AS_STR(v)   ((aupStr *)AUP_AS_OBJ(v))
+#define AUP_AS_CSTR(v)  (AS_STR(v)->chars)
+#define AUP_AS_FUN(v)   ((aupFun *)AUP_AS_OBJ(v))
+#define AUP_AS_MAP(v)   ((aupMap *)AUP_AS_OBJ(v))
+
+static inline bool aup_isObject(aupVal value, aupOType type) {
+    return AUP_IS_OBJ(value) && AUP_OBJTYPE(value) == type;
+}
+
+void aup_printObject(aupObj *object);
+const char *aup_typeofObject(aupObj *object);
+void aup_freeObject(aupGC *gc, aupObj *object);
+
+aupStr *aup_takeString(aupVM *vm, char *chars, int length);
+aupStr *aup_copyString(aupVM *vm, const char *chars, int length);
+
+aupFun *aup_newFunction(aupVM *vm, aupSrc *source);
+
+aupMap *aup_newMap(aupVM *vm);
