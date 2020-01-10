3bbeb57ad466eb2d00aa3eb9b53105b6d1301acf
wy3 <wuuyi123@gmail.com>
2020-01-10 10:18:02 +0700

    update main

--- src/main.c
+++ src/main.c
@@ -1,7 +1,21 @@
 #include <stdio.h>
 
+#include "vm.h"
+
 int main(int argc, char **argv)
 {
+    if (argc < 2) {
+        printf("Usage: aup [file]\n");
+        return 0;
+    }
 
-    return 0;
+    aupVM *vm = aup_create();
+    int ret = AUP_INIT_ERROR;
+
+    if (vm != NULL) {
+        ret = aup_doFile(vm, argv[argc - 1]);
+        aup_close(vm);
+    }
+
+    return ret;
 }
